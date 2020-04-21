a474840018d6be5419b39dafa7caa3738a2d0ec8
adi618 <59510889+adi618@users.noreply.github.com>
2020-04-21 21:13:08 +0200

    update

--- vjezbanje/vjezbanje/Source.cpp
+++ vjezbanje/vjezbanje/Source.cpp
@@ -4,8 +4,9 @@ void Adi_Zubic_Zadaca_Z1_ZaDobrovoljce();
 void Adi_Zubic_Zadaca_Z2_ZaDobrovoljce();
 void Adi_Zubic_Zadaca_Z3_ZaDobrovoljce();
 void Adi_Zubic_Zadaca_Z5_ZaDobrovoljce();
+void Adi_Zubic_Zadaca_5_Z4();
 
 int main()
 {
-	Adi_Zubic_Zadaca_Z5_ZaDobrovoljce();
+	Adi_Zubic_Zadaca_5_Z4();
 }
