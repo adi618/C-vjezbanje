d948dcc699609ea56e59f0f999df4aa56d01fc88
adi618 <59510889+adi618@users.noreply.github.com>
2020-04-22 19:12:21 +0200

    update

--- vjezbanje/vjezbanje/Adi_Zubic_Zadaca_Z1_ZaDobrovoljce.cpp
+++ vjezbanje/vjezbanje/Adi_Zubic_Zadaca_Z1_ZaDobrovoljce.cpp
@@ -16,7 +16,7 @@ void Adi_Zubic_Zadaca_Z1_ZaDobrovoljce()
             izlaznaRecenica += ulaznaRecenica + '\n';       // uzimamo svaku liniju iz file-a i stavljamo je u string izlaznaRecenica
         }
         
-        for (auto& rijec : izlaznaRecenica)     // iteriramo slova recenice i sifriramo ih, u for petlju je proslijedjena rijec po referenci
+        for (char& rijec : izlaznaRecenica)     // iteriramo slova recenice i sifriramo ih, u for petlju je proslijedjena rijec po referenci
         {                                       // da nije po referenci, petlja bi samo napravila kopiju rijeci i editovanje te kopije
             if (rijec >= 'A' && rijec <= 'Z')   // ne bi imalo efekta na originalnu poruku
                 if (rijec == 'Z')
@@ -24,13 +24,10 @@ void Adi_Zubic_Zadaca_Z1_ZaDobrovoljce()
                 else
                     rijec++;
         }
-
-        file.close();               // zatvaramo file, buduci da smo zavrsili sa ucitavanjem i ne treba nam vise, mogli smo ovo uraditi
-        remove("tekst.txt");        // odmah nakon while (getline) petlje.
-
-	    std::ofstream file("tekst.txt");    // pravimo novi file u koji cemo samo pisati, dakle ofstream, pod imenom tekst.txt
-        file << izlaznaRecenica;            // sifriranu recenicu spasavamo u njeg
-        file.close();                       // zatvaramo ga
+        file.close();                       // zatvaramo file
+	    std::ofstream file("tekst.txt");    // otvaramo ga u output file stream mode-u, sto brise sav tekst iz file-a
+        file << izlaznaRecenica;            // u, sada prazan, file upisujemo nasu sifriranu recenicu
+        file.close();                       // zatvaramo file.
 
         std::cout << "\n\n\tTekst je ucitan i velika slova su sifrirana!\n\n\n";
     }
